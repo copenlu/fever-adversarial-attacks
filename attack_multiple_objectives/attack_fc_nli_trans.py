Refactor to use all objectives and fix trigger and gradient position handling.

attack_multiple_objectives/attack_fc_nli_trans.py
@@ -20,21 +20,39 @@ from attack_multiple_objectives import triggers_utils
 from attack_multiple_objectives.nli_utils import collate_nli_tok_ids
 
 
-def get_nli_model(model_path, tokenizer, device='cpu'):
-    checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
-    nli_args = argparse.Namespace(**checkpoint['args'])
-    transformer_config = BertConfig.from_pretrained('bert-base-uncased', num_labels=nli_args.labels)
-    nli_model = BertForSequenceClassification.from_pretrained('bert-base-uncased', config=transformer_config).to(device)
-    nli_model.load_state_dict(checkpoint['model'])
-    nli_model = nli_model.to(device)
-    nli_model_ew = triggers_utils.get_embedding_weight_bert(nli_model)
-    collate_nli = partial(collate_nli_tok_ids, tokenizer=tokenizer, device=device)
-    triggers_utils.add_hooks_bert(nli_model)
+collate_functions = {"roberta-large-mnli": collate_nli_tok_ids,
+                     "SparkBeyond/roberta-large-sts-b": collate_nli_tok_ids,
+                     'roberta-large-openai-detector': collate_nli_tok_ids}
 
-    return nli_model, nli_model_ew, collate_nli
+mnli_classes = {'entailment': 2, 'negation': 0, 'neutral': 1}
+gpt_detector_classes = {'real': 1, 'fake': 0}
 
 
-def get_fc_model(model_path, tokenizer, labels=3, device='cpu'):
+def get_checkpoint_transformer(model_name, device, hook_embeddings=False, model_type='bert'):
+    """
+    'roberta-large-openai-detector' output: (tensor([[-0.1055, -0.6401]], grad_fn=<AddmmBackward>),)
+    Real-1, Fake-0
+    Note from the authors: 'The results start to get reliable after around 50 tokens.'
+
+    "SparkBeyond/roberta-large-sts-b" model output: (tensor([[0.6732]], grad_fn=<AddmmBackward>),)
+    STS-B benchmark measure the relatedness of two sentences based on the cosine similarity of the two representations
+
+    roberta-large-mnli output: (tensor([[-1.8364,  1.4850,  0.7020]], grad_fn=<AddmmBackward>),)
+    contradiction-0, neutral-1, entailment-2
+    """
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name).to(device)
+    collate = partial(collate_functions[model_name], tokenizer=tokenizer, device=device)
+
+    model_ew = None
+    if hook_embeddings:
+        model_ew = triggers_utils.get_embedding_weight_bert(model, model_type)
+        triggers_utils.add_hooks_bert(model, model_type)
+
+    return model, model_ew, collate, tokenizer
+
+
+def get_fc_model(model_path, tokenizer, labels=3, device='cpu', type='bert'):
     collate_fn = partial(collate_fever, tokenizer=tokenizer, device=device)
 
     if type == 'bert':
@@ -48,20 +66,10 @@ def get_fc_model(model_path, tokenizer, labels=3, device='cpu'):
     model.load_state_dict(checkpoint['model'])
     model.train()  # rnn cannot do backwards in train mode
 
-    triggers_utils.add_hooks_bert(model, args.fc_model_type)  # Adds a hook to get the embedding gradients
-    embedding_weight = triggers_utils.get_embedding_weight_bert(model, args.fc_model_type)
+    triggers_utils.add_hooks_bert(model,type)  # Adds a hook to get the embedding gradients
+    embedding_weight = triggers_utils.get_embedding_weight_bert(model, type)
 
     return model, embedding_weight, collate_fn
-
-
-def get_ppl_model(device='cpu'):
-    model = BertForMaskedLM.from_pretrained('bert-base-uncased').to(device)
-    model.train()
-
-    triggers_utils.add_hooks_bert(model)  # Adds a hook to get the embedding gradients
-    embedding_weight = triggers_utils.get_embedding_weight_bert(model)
-
-    return model, embedding_weight
 
 
 if __name__ == "__main__":
@@ -77,8 +85,6 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", help="Number of epochs to run trigger optimisation for.", type=int, default=3)
     parser.add_argument("--labels", help="2 labels if NOT ENOUGH INFO excluded, 3 otherwise", type=int, default=3)
     parser.add_argument("--trigger_length", help="The total length of the trigger", type=int, default=1)
-    parser.add_argument("--nli_model_path", help="Path to the fine-tuned NLI model", default='snli_transformer',
-                        type=str)
     parser.add_argument("--fc_w", help="The total length of the trigger", type=float, default=1.0)
     parser.add_argument("--nli_w", help="The total length of the trigger", type=float, default=0.0)
     parser.add_argument("--ppl_w", help="The total length of the trigger", type=float, default=0.0)
@@ -95,13 +101,26 @@ if __name__ == "__main__":
     print(args, flush=True)
 
     # load models
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    if args.fc_model_type == 'bert':
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    else:
+        tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
-    nli_model, nli_model_ew, collate_nli = get_nli_model(args.nli_model_path, tokenizer, device)
-    fc_model, fc_model_ew, collate_fc = get_fc_model(args.model_path, tokenizer, args.labels, device)
-    ppl_model, ppl_ew = get_ppl_model(device)
+    nli_model, nli_model_ew, nli_batch_func, gpt_model, gpt_model_ew, gpt_batch_func = None, None, None, None, None, None
+    if args.nli_w > 0:
+        nli_model, nli_model_ew, _, _ = \
+        get_checkpoint_transformer("roberta-large-mnli", device, hook_embeddings=True, model_type='roberta')
+        nli_batch_func = partial(triggers_utils.evaluate_batch_nli, tokenizer=tokenizer)
+
+    if args.ppl_w > 0:
+        gpt_model, gpt_model_ew, _, _ =\
+        get_checkpoint_transformer("roberta-large-openai-detector", device, hook_embeddings=True, model_type='roberta')
+        gpt_batch_func = partial(triggers_utils.evaluate_batch_gpt, tokenizer=tokenizer)
+
+    fc_model, fc_model_ew, collate_fc = get_fc_model(args.model_path, tokenizer, args.labels, device, type=args.fc_model_type)
 
     test = FeverDataset(args.dataset)
+
     # Subsample the dataset
     test._dataset = [i for i in test._dataset if i['label'] == args.attack_class]
     target_label = label_map[args.target]
@@ -111,61 +130,57 @@ if __name__ == "__main__":
     # Initialize attack_multiple_objectives
     num_trigger_tokens = args.trigger_length
     batch_triggers = []
-    trigger_token_ids = tokenizer.convert_tokens_to_ids(["[MASK]"]) * num_trigger_tokens
+    trigger_token_ids = tokenizer.convert_tokens_to_ids(["a"]) * num_trigger_tokens
+
+    print('Dataset size', len(test._dataset), flush=True)
+
+    previous_triggers, trigger_counts = None, None
     for e in range(args.epochs):
         trigger_counts = defaultdict(int)
-        for i, batch in tqdm(enumerate(test_dl)):
-            # TODO: decide if the attacks will be batch-specific
-            # if e == 0:
-            #     trigger_token_ids = tokenizer.convert_tokens_to_ids(["[MASK]"]) * num_trigger_tokens
-            #     batch_triggers.append(trigger_token_ids)
-            # else:
-            #     trigger_token_ids = batch_triggers[i]
 
+        for i, batch in tqdm(enumerate(test_dl)):
             fc_model.train()  # rnn cannot do backwards in train mode
 
-            # get grad of attack_multiple_objectives
-            # trigger_token_ids = [get_candidate_mask_tokens(batch[0], nlp, num_trigger_tokens)[0][0]]
+            averaged_grad = triggers_utils.get_average_grad_transformer(fc_model, batch, trigger_token_ids,
+                                                                        triggers_utils.evaluate_batch_bert, target_label)
+            avg_grad_nli, avg_grad_gpt = None, None
+            if args.nli_w > 0:
+                avg_grad_nli = triggers_utils.get_average_grad_transformer(nli_model, batch, trigger_token_ids,
+                                                                           nli_batch_func, mnli_classes['entailment'])
+            if args.ppl_w > 0:
+                avg_grad_gpt = triggers_utils.get_average_grad_transformer(nli_model, batch, trigger_token_ids,
+                                                                           gpt_batch_func, gpt_detector_classes['real'])
 
-            averaged_grad = triggers_utils.get_average_grad_bert(fc_model, batch, trigger_token_ids, target_label)
-            avg_grad_nli = triggers_utils.get_average_grad_bert_nli(nli_model, batch, trigger_token_ids, tokenizer)
-            avg_grad_ppl = triggers_utils.get_average_grad_bert_ppl(ppl_model, batch, trigger_token_ids, tokenizer)
-
-            # find attack candidates using an attack method
-            # cand_trigger_token_ids = attacks.hotflip_attack(averaged_grad,
-            #                                                 fc_model_ew,
-            #                                                 trigger_token_ids,
-            #                                                 num_candidates=50)
-            # cand_trigger_token_ids = attacks.hotflip_attack_nli(averaged_grad, fc_model_ew,
-            #                                                     avg_grad_nli, nli_model_ew,
-            #                                                     num_candidates=40,
-            #                                                     fc_w=0.5, nli_w=0.5)
             cand_trigger_token_ids = attacks.hotflip_attack_all(averaged_grad, fc_model_ew,
                                                                 avg_grad_nli, nli_model_ew,
-                                                                avg_grad_ppl, ppl_ew,
+                                                                avg_grad_gpt, gpt_model_ew,
                                                                 nli_w=args.nli_w, fc_w=args.fc_w, ppl_w=args.ppl_w,
-                                                                num_candidates=10)
+                                                                num_candidates=30)
 
-            # query the model to get the best candidates
-            trigger_token_ids = triggers_utils.get_best_candidates_bert(fc_model,
-                                                                        batch,
-                                                                        trigger_token_ids,
-                                                                        cand_trigger_token_ids)
-            # batch_triggers[i] = trigger_token_ids
+            trigger_token_ids = triggers_utils.get_best_candidates_all_obj(fc_model, nli_model, gpt_model, batch,
+                                                                           trigger_token_ids, cand_trigger_token_ids,
+                                                                           tokenizer,
+                                                                           nli_w=args.nli_w, fc_w=args.fc_w, ppl_w=args.ppl_w, beam_size=2)
+
+
             new_trigger = tokenizer.convert_ids_to_tokens(trigger_token_ids)
             trigger_counts[" ".join(new_trigger)] += 1
 
             gc.collect()
-        print(list(sorted(trigger_counts.items(), key=lambda x: x[1], reverse=True))[:20], flush=True)
+
+        best_epoch_triggers = list(sorted(trigger_counts.items(), key=lambda x: x[1], reverse=True))[:20]
+        if e > 3 and best_epoch_triggers[0][1] > 200:
+            break
+
+        print(best_epoch_triggers, flush=True)
 
     # Rank all of the attack_multiple_objectives based on the number of times they were selected
     ranked_triggers = list(sorted(trigger_counts.items(), key=lambda x: x[1], reverse=True))
     print(ranked_triggers, flush=True)
     os.makedirs('./attack_results', exist_ok=True)
-    with open(f'./attack_results/fc{int(args.fc_w*10)}_nli{int(args.nli_w*10)}_ppl{int(args.ppl_w*10)}_{args.attack_class}_to_{args.target}_{num_trigger_tokens}_triggers.tsv',
+    with open(f'./attack_results/fc{int(args.fc_w*10)}_nli{int(args.nli_w*10)}_ppl{int(args.ppl_w*10)}'
+              f'_{args.attack_class}_to_{args.target}_{num_trigger_tokens}_triggers.tsv',
               'wt') as f:
         for trigger, count in ranked_triggers:
             trigger_token_ids = tokenizer.convert_tokens_to_ids(trigger.split(" "))
-            # acc = triggers_utils.eval_model(fc_model, test_dl, trigger_token_ids, labels_num=args.labels)
-            # pred_dict_orig, pred_dict, prob_entail = triggers_utils.eval_nli(nli_model, test_dl, tokenizer, trigger_token_ids)
             f.write(f"{trigger}\t{count}\n")

attack_multiple_objectives/eval_triggers.py
@@ -1,14 +1,25 @@
 import argparse
-import random
+import torch
 
 import pandas as pd
 from transformers import BertTokenizer
-from transformers import pipeline
+from functools import partial
 
 from attack_multiple_objectives import triggers_utils
-from attack_multiple_objectives.attack_fc_nli_trans import get_fc_model, get_nli_model, get_ppl_model
-from attack_multiple_objectives.nli_utils import NLI_DIC_LABELS
+from attack_multiple_objectives.attack_fc_nli_trans import get_fc_model, get_checkpoint_transformer, mnli_classes
 from builders.data_loader import BucketBatchSampler, sort_key, FeverDataset
+from transformers import RobertaTokenizer, RobertaForMaskedLM
+
+
+def get_ppl_model(device='cpu'):
+    model = RobertaForMaskedLM.from_pretrained('roberta-base').to(device)
+    model.train()
+
+    triggers_utils.add_hooks_bert(model)  # Adds a hook to get the embedding gradients
+    embedding_weight = triggers_utils.get_embedding_weight_bert(model)
+
+    return model, embedding_weight
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -23,53 +34,51 @@ if __name__ == "__main__":
     parser.add_argument("--labels", help="2 labels if NOT ENOUGH INFO excluded, 3 otherwise", type=int, default=3)
     parser.add_argument("--nli_model_path", help="Path to the fine-tuned NLI model", default='snli_transformer',
                         type=str)
+    parser.add_argument("--fc_model_type", help="Type of pretrained model being loaded", default='bert',
+                        choices=['bert', 'roberta'])
+
     args = parser.parse_args()
 
-    device = 0 if args.gpu else -1
+    device = torch.device("cuda") if args.gpu else torch.device("cpu")
 
     # load models
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    nlp = pipeline('fill-mask', model='bert-base-uncased', device=device, topk=args.beam_size)
+    if args.fc_model_type == 'bert':
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    else:
+        tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
-    nli_model, nli_model_ew, collate_nli = get_nli_model(args.nli_model_path, tokenizer, device)
-    fc_model, fc_model_ew, collate_fc = get_fc_model(args.model_path, tokenizer, args.labels, device)
-    ppl_model, ppl_ew = get_ppl_model(device)
+    fc_model, fc_model_ew, collate_fc = get_fc_model(args.model_path, tokenizer, args.labels, device,
+                                                     type=args.fc_model_type)
 
     test = FeverDataset(args.dataset)
-    # Subsample the dataset
-    test._dataset = random.sample([i for i in test._dataset if i['label'] == args.attack_class], 200)
+    test._dataset = [i for i in test._dataset if i['label'] == args.attack_class]
     test_dl = BucketBatchSampler(batch_size=args.batch_size, sort_key=sort_key, dataset=test,
                                  collate_fn=collate_fc)
 
+    nli_model, nli_model_ew, _, _ = \
+        get_checkpoint_transformer("roberta-large-mnli", device, hook_embeddings=True, model_type='roberta')
+    nli_batch_func = partial(triggers_utils.evaluate_batch_nli, tokenizer=tokenizer)
+
+
+    ppl_batch_func = partial(triggers_utils.evaluate_batch_ppl, tokenizer=tokenizer)
+    ppl_model = get_ppl_model(device)
+
     triggers = pd.read_csv(args.triggers_file, sep='\t', header=None)
-    triggers.sort_values(by=2, axis=0, ascending=False)
-    triggers.columns = ['trigger', 'count', 'acc', 'nli']
+    triggers.columns = ['trigger', 'count']
 
-    print("Getting original performance...")
-    orig_acc = triggers_utils.eval_model(fc_model, test_dl, labels_num=args.labels)
+    orig_acc = triggers_utils.eval_fc(fc_model, test_dl, labels_num=args.labels)
     print(f'Original accuracy: {orig_acc}', flush=True)
-
-    # TODO add the GLUE functionality here
-
+    orig_nli = triggers_utils.eval_nli(nli_model, test_dl, tokenizer, trigger_token_ids=None)
+    print(f'Original nli class distrib: {orig_nli}', flush=True)
+    ppl_orig, ppl_std = triggers_utils.eval_ppl(ppl_model, test_dl, tokenizer, trigger_token_ids=None)
+    print(f'Original ppl: {ppl_orig}{ppl_std}', flush=True)
     with open(args.triggers_file + '_results', 'w') as f:
         for i, row in triggers.iterrows():
             row = row.to_dict()
             trigger = row['trigger']
             trigger_token_ids = tokenizer.convert_tokens_to_ids(trigger.split(" "))
-            acc = triggers_utils.eval_model(fc_model, test_dl, trigger_token_ids, labels_num=args.labels)
-
-            pred_dict_orig, pred_dict, prob_entail = triggers_utils.eval_nli(nli_model, test_dl, tokenizer,
-                                                                             trigger_token_ids)
-
-            ppl_loss_orig = triggers_utils.eval_ppl(ppl_model, test_dl, tokenizer)
-            ppl_loss = triggers_utils.eval_ppl(ppl_model, test_dl, tokenizer, trigger_token_ids)
-
-            delta_ppl = ppl_loss - ppl_loss_orig  # the higher, the better
-            delta_acc = orig_acc - acc  # the higher, the better
-            all_instances = sum([v for _, v in pred_dict_orig.items()])
-            delta_ent = (pred_dict_orig[NLI_DIC_LABELS['entailment']] - pred_dict[NLI_DIC_LABELS['entailment']])
-            delta_neutr = (pred_dict_orig[NLI_DIC_LABELS['neutral']] - pred_dict[NLI_DIC_LABELS['neutral']])
-            delta_neg = (pred_dict_orig[NLI_DIC_LABELS['contradiction']] - pred_dict[NLI_DIC_LABELS['contradiction']])
-
-            f.write(f"{trigger}\t{delta_acc}\t{delta_ent}\t{delta_neutr}\t{delta_neg}\t{prob_entail}\t{delta_ppl}\n")
+            acc = triggers_utils.eval_fc(fc_model, test_dl, trigger_token_ids, labels_num=args.labels)
+            pred_dict = triggers_utils.eval_nli(nli_model, test_dl, tokenizer, trigger_token_ids)
+            ppl_loss, ppl_std = triggers_utils.eval_ppl(ppl_model, test_dl, tokenizer, trigger_token_ids)
+            f.write(f"{trigger}\t{acc}\t{mnli_classes['entailment']}\t{ppl_loss}({ppl_std})\n")
             f.flush()